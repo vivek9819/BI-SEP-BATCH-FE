371fe1427a8c67e7e7b81220236151a9b3f9d8bd
vivek9819 <97894199+vivek9819@users.noreply.github.com>
2022-10-11 21:37:33 +0530

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+/*let department=prompt("enter department name")
+if(department==="ces"){
+    console.log("goto secont floor")
+}
+else if(department==="mech"){
+    console.log("goto third floor")
+}
+else if(department==="ece"){
+    console.log("goto third floor")
+}
+else{
+    console.log("wrong input")
+}*/
+
+ 
+ 
